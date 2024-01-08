010b3fc25b3b70850c92c6a8f1bb10df50d1bfe6
Pablo Roberto <pablomoura2016@gmail.com>
2024-01-08 16:27:43 -0300

    manipulando com dom

---
+++ Fokus-aula-1/script.js
@@ -0,0 +1,23 @@
+const html = document.querySelector('html')
+const focoBt = document.querySelector('.app__card-button--foco')
+const curtoBt = document.querySelector('.app__card-button--curto')
+const longoBt = document.querySelector('.app__card-button--longo')
+const banner = document.querySelector('.app__image')
+
+focoBt.addEventListener('click', () => {
+    alteraContexto('foco')
+})
+
+curtoBt.addEventListener('click', () => {
+    alteraContexto('descanso-curto')
+})
+
+longoBt.addEventListener('click', () => {
+    alteraContexto('descanso-longo')
+})
+
+function alteraContexto(contexto){
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
+}
+
