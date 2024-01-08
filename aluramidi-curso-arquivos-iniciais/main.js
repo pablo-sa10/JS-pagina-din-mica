b78bf32075120daf3be9c221b43eef4921fa4b20
Pablo Roberto <pablomoura2016@gmail.com>
2024-01-08 16:28:09 -0300

    ok

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,17 +1,33 @@
-function tocaSom(idElementoAudio){
+function tocaSom(idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
 
 const listaTeclas = document.querySelectorAll('.tecla');
 
-for(let contador = 0; contador < listaTeclas.length; contador++){
+for (let contador = 0; contador < listaTeclas.length; contador++) {
 
     const tecla = listaTeclas[contador];
-    const instrumento = tecla.classList[1];  
+    const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
 
-    tecla.onclick = function(){
+    tecla.onclick = function () {
         tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+       /*  if(evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        } */
+    }
+
+    tecla.onkeyup = function (evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.remove('ativa');
+        } 
     }
 }
 
